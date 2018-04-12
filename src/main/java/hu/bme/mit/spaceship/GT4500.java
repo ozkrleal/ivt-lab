0466afd2d7f99791ffc66996a5dc2c9853f570f5
Oscar Leal <ozkralm10@hotmail.com>
2018-04-12 00:27:44 -0700

    I fixerd the errrersaa

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,6 +79,9 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
+	if (! secondaryTorpedoStore.isEmpty() && ! primaryTorpedoStore.isEmpty()) {
+              firingSuccess = secondaryTorpedoStore.fire(1);
+	      firingSuccess = primaryTorpedoStore.fire(1);
 
         break;
     }
